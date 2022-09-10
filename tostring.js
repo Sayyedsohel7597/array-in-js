35f44ba574285d49a8c146f123aa2348056ee16d
sayyed <md.sohel7597@gmail.com>
2022-09-10 11:56:28 +0530

    "ho gae array methods"

---
+++ tostring.js
@@ -0,0 +1,9 @@
+var arr = ["sohel", "sayyed", "salim", "yusuf", "mir"];
+b = arr.toString();
+// console.log(b); //it converts array to string
+//sohel,sayyed,salim,yusuf,mir
+// c = arr.valueOf()
+// console.log(c)
+
+d = arr.fill("empire");  //it fill to all element as given---like--[ 'empire', 'empire', 'empire', 'empire', 'empire' ]
+console.log(d)
